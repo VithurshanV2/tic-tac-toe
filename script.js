6df6dd86ac864fb48d823daf8793468fa61ad513
vithurshan <164380624+VithurshanV2@users.noreply.github.com>
2025-05-21 19:44:47 +0530

    Add player function to store name and marker

--- script.js
+++ script.js
@@ -28,3 +28,10 @@ function Cell() {
 
     return { getValue, setValue };
 }
+
+function Player(name, marker) {
+    const getName = () => name;
+    const getMarker = () => marker;
+
+    return { getName, getMarker };
+}
