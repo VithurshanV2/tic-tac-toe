7572d4081539de206c6e6e4d2779b096b23382cc
vithurshan <164380624+VithurshanV2@users.noreply.github.com>
2025-05-22 18:50:37 +0530

    Add setCell method and GameController function
    - add playRound function within GameController
    - make minor changes in Cell function
    - add setCell method inside Gameboard

--- script.js
+++ script.js
@@ -12,17 +12,20 @@ function Gameboard() {
 
     const getBoard = () => board;
 
-    return { getBoard };
+    const setCell = (row, col, marker) => {
+        board[row][col].setValue(marker);
+    };
+    return { getBoard, setCell };
 }
 
 function Cell() {
-    let value = 0;
+    let value = "";
 
     const getValue = () => value;
 
     const setValue = (newValue) => {
         if (value === "") {
-            value.newValue;
+            value = newValue;
         }
     };
 
@@ -34,4 +37,20 @@ function Player(name, marker) {
     const getMarker = () => marker;
 
     return { getName, getMarker };
+}
+
+function GameController() {
+    const player1 = Player("Player 1", "X");
+    const player2 = Player("Player 2", "O");
+    const board = Gameboard();
+
+    let currentPlayer = player1;
+
+    function playRound(row, col) {
+        const marker = currentPlayer.getMarker();
+
+        board.setCell(row, col, marker);
+
+        currentPlayer = currentPlayer === player1 ? player2 : player1;
+    }
 }
